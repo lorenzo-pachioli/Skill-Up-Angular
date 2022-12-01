c0dc066a0cd7ce7733baa8010a96fec0661fb77e
Maxi <sebastianfermaxi@gmail.com>
2022-12-01 20:25:18 -0300

    componente titulo

--- src/app/shared/components/titulo/titulo.component.ts
+++ src/app/shared/components/titulo/titulo.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
-  selector: 'app-titulo',
+  selector: 'ew-titulo',
   templateUrl: './titulo.component.html',
   styleUrls: ['./titulo.component.scss']
 })
 export class TituloComponent implements OnInit {
+  @Input() titulo: string ='Ingrese título';
 
   constructor() { }
 
