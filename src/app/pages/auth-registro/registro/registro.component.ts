724a2f5f75a03d18bed17a97ce25ff1bd0b9cb23
Anacoronel <anacoronel@hotmail.com>
2022-12-01 10:55:54 -0300

    validator reg works

--- src/app/pages/auth-registro/registro/registro.component.ts
+++ src/app/pages/auth-registro/registro/registro.component.ts
@@ -18,17 +18,18 @@ export class RegistroComponent implements OnInit {
     private router:Router,
     private fb:FormBuilder
   ) { this.registerForm = new FormGroup({
+    firstname: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email,Validators.pattern(
       '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,63}$',
     ),]),
     password: new FormControl('', [Validators.required,Validators.pattern(
       '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
-    ),]),
-    firstname: new FormControl('', [Validators.required]),
-    name: new FormControl('', [Validators.required])
+    )])
   });
-}
+ }
     
+  
 
     
   
