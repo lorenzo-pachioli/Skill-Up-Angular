e9dcfdede77236b4acbcd3c391a2e27608a4737e
Anacoronel <anacoronel@hotmail.com>
2022-12-01 01:47:31 -0300

    SUA-27-formulario-de-login

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,7 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatError, MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -44,7 +44,8 @@ import { MatDividerModule } from '@angular/material/divider';
 		MatPaginatorModule,
 		MatTableModule,
 		MatSortModule,
-		MatDividerModule
+		MatDividerModule,
+		MatError
 	],
 	exports: [
 		MatSnackBarModule,
@@ -67,7 +68,8 @@ import { MatDividerModule } from '@angular/material/divider';
 		MatPaginatorModule,
 		MatTableModule,
 		MatSortModule,
-		MatDividerModule
+		MatDividerModule,
+		MatError
 	]
 })
 export class MaterialModule {
--- src/app/pages/auth-login/auth-login.module.ts
+++ src/app/pages/auth-login/auth-login.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AuthLoginRoutingModule } from './auth-login-routing.module';
 import { LoginComponent } from './login/login.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +14,12 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    AuthLoginRoutingModule
+    AuthLoginRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class AuthLoginModule { }
