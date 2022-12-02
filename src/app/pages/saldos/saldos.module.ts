4d5e851aee0d089c35ddef77b607c8a0f59e049e
Maxi <sebastianfermaxi@gmail.com>
2022-12-02 19:40:44 -0300

    titulo2

--- src/app/pages/saldos/saldos.module.ts
+++ src/app/pages/saldos/saldos.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SaldosRoutingModule } from './saldos-routing.module';
 import { SaldosComponent } from './saldos-page/saldos.component';
+import { SaldoFormCreateEditComponent } from './saldo-form-create-edit/saldo-form-create-edit.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/material/material.module';
 
 @NgModule({
-  declarations: [SaldosComponent],
-  imports: [CommonModule, SaldosRoutingModule],
+  declarations: [SaldosComponent, SaldoFormCreateEditComponent],
+  imports: [
+    CommonModule,
+    SaldosRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ],
 })
 export class SaldosModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -24,6 +24,6 @@ import { AlertComponent } from './components/alert/alert.component';
     AlertComponent,
   ],
   imports: [CommonModule, MaterialModule],
-  exports: [TableComponent, FooterComponent, LoaderComponent],
+  exports: [TableComponent, FooterComponent, LoaderComponent, TituloComponent],
 })
 export class SharedModule { }
