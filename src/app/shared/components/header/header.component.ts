6aa67c7c45f6cdf54462b61d0529e3e568396856
Ariana Osuna <arianaosunavargas92@gmail.com>
2022-12-03 11:26:58 -0300

    ajustes componente header y cambio de favicon

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSidenav } from "@angular/material/sidenav";
+import { BreakpointObserver } from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  events: string[] = [];
+  opened!: boolean;
 
-  ngOnInit(): void {
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  toggle(nav: MatSidenav) {
+    const isSmallScreen = this.breakpointObserver.isMatched(
+      "(max-width: 599px)"
+    );
+    if (isSmallScreen) {
+      nav.toggle();
+    }
   }
-
+  
   item: any;
 
   onClick(item:any){
