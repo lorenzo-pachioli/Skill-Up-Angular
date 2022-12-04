4c623d3c70ca2c248c75baca802c5aecc005666a
Ariana Osuna <arianaosunavargas92@gmail.com>
2022-12-04 19:44:43 -0300

    creacion de componente perfil-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { SharedModule } from './shared/shared.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInyectorInterceptor } from './core/interceptors/token-inyector.interceptor';
 import { PerfilDetailModule } from './pages/usuarios/perfil-detail/perfil-detail.module';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
