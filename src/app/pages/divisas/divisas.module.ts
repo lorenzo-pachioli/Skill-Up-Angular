021fbe1ff21150a91ed89cff995204bfe614ede4
Anacoronel <anacoronel@hotmail.com>
2022-12-06 12:24:24 -0300

    SUA-71-Comp-cotiz-cambio-del-dia

--- src/app/pages/divisas/divisas.module.ts
+++ src/app/pages/divisas/divisas.module.ts
@@ -4,9 +4,10 @@ import { MaterialModule } from 'src/app/material/material.module';
 
 import { DivisasRoutingModule } from './divisas-routing.module';
 import { DivisasComponent } from './divisas-page/divisas.component';
+import { ExchangeComponent } from './exchange/exchange.component';
 
 @NgModule({
-  declarations: [DivisasComponent],
+  declarations: [DivisasComponent, ExchangeComponent],
   imports: [CommonModule, DivisasRoutingModule, MaterialModule],
 })
 export class DivisasModule {}
